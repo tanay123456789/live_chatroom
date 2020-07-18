c3e7f24dd0a14861a826de1aab2dd8ac616cff89
Tanay Dwivedi <tanaydwivedi456@gmail.com>
2020-07-18 08:26:28 +0530

    Added main.html

--- main.js
+++ main.js
@@ -1,31 +1,29 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const app = require("express")();
+    const http = require("http").Server(app);
+    const io = require("socket.io")(http);
+    const port = process.env.PORT || 3000;
 
-app.get('/', function(req, res) {
-   res.sendFile(__dirname + '/main.html');
-});
+    app.get("/", function(req, res) {
+        res.sendFile(__dirname + "/main.html");
+    });
 
-users = [];
-io.on('connection', function(socket) {
-   console.log('A user connected');
-   socket.on('setUsername', function(data) {
-      console.log(data);
-      if(users.indexOf(data) > -1) {
-         socket.emit('userExists', data + ' username is taken! Try some other username.');
-      } 
-      else {
-         users.push(data);
-         socket.emit('userSet', {username: data});
-      }
-   });
-   
-   socket.on('msg', function(data) {
-      //Send message to everyone
-      io.sockets.emit('newmsg', data);
-   })
-});
+    io.on("connection", function(socket) {
 
-http.listen(8080, function() {
-   console.log('listening on localhost:8080');
-});
+        socket.on("user_join", function(data) {
+            this.username = data;
+            socket.broadcast.emit("user_join", data);
+        });
+
+        socket.on("chat_message", function(data) {
+            data.username = this.username;
+            socket.broadcast.emit("chat_message", data);
+        });
+
+        socket.on("disconnect", function(data) {
+            socket.broadcast.emit("user_leave", this.username);
+    	});
+    });
+
+    http.listen(port, function() {
+        console.log("Listening on *:" + port);
+    });
